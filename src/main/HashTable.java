7d15ea0357899cec3137827c3cb90f78c1d82419
Nicholas Esposito <nesposi3@oswego.edu>
2019-09-30 14:41:38 -0400

    Created hashtable for tf-idf

---
+++ src/main/HashTable.java
@@ -0,0 +1,108 @@
+package main;
+
+public class HashTable {
+    public static final double RESIZE_RATIO=.75;
+    public static final int INITIAL_SIZE=8;
+    public static class Node{
+        public Node(String key){
+            this.key = key;
+            this.hashCode = key.hashCode();
+            this.wordFrequency = 1;
+            this.next = null;
+        }
+        public String key;
+        public int wordFrequency;
+        public int hashCode;
+        public Node next;
+    }
+    private int numKeys;
+    private Node[] arr;
+
+    public Node get(String key){
+        return new Node(key);
+    }
+    private void resize(){
+        int newArrSize = arr.length*2;
+        Node[] newArr = new Node[newArrSize];
+        for (int i = 0; i < arr.length ; i++) {
+            Node n = arr[i];
+            while(n!=null){
+                int h = n.hashCode;
+                int newIndex = h & (newArrSize-1);
+                if(newArr[newIndex]!=null){
+                    newArr[newIndex].next = n;
+                }else{
+                    newArr[newIndex] = n;
+                }
+                n=n.next;
+            }
+        }
+        this.arr = newArr;
+    }
+    public boolean keyExists(String key){
+        int h = key.hashCode();
+        int index = h & ((arr.length)-1);
+        Node n = arr[index];
+        while (n!=null){
+            if(n.key.equals(key)){
+                return true;
+            }
+            n=n.next;
+        }
+        return false;
+    }
+    public void add(String key){
+        int h = key.hashCode();
+        int index = h & ((arr.length) - 1);
+        if(keyExists(key)){
+            //Node already exists, increase frequency by one
+            Node n = arr[index];
+            while (n!=null){
+                if(n.key.equals(key)){
+                    n.wordFrequency ++;
+                }
+                n=n.next;
+            }
+        }else{
+            //Node does not exist. Add new node and resize if necessary.
+            Node n = new Node(key);
+            if(arr[index]==null){
+                arr[index] = n;
+            }else{
+                arr[index].next = n;
+            }
+            this.numKeys++;
+            double ratio =  ((double) numKeys / (double) arr.length);
+            if(ratio>=RESIZE_RATIO){
+                // Resize necessary
+                resize();
+            }
+        }
+
+    }
+    public void printAll(){
+        for (int i = 0; i < arr.length; i++) {
+            Node n = arr[i];
+            while (n!=null){
+                System.out.println(n.key + " " + n.wordFrequency);
+                n=n.next;
+            }
+        }
+    }
+    public HashTable(){
+        this.arr = new Node[INITIAL_SIZE];
+        this.numKeys = 0;
+    }
+    public static void main(String[] args){
+        HashTable table = new HashTable();
+        table.add("hello");
+        table.add("howdy");
+        System.out.println(table.arr.length);
+        table.add("aewe");
+        table.add("dwewqedsad");
+        table.add("dnjnjknjk");
+        table.add("meow");
+        System.out.println(table.arr.length);
+        table.printAll();
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -12,6 +12,8 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -21,8 +23,36 @@ import java.net.MalformedURLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import main.HashTable;
+import main.HashTable.Node;
 
 public class Main extends Application {
+    public final static String BASE_URI = "https://en.wikipedia.org";
+    public final static Pattern URL_PATTERN = Pattern.compile("\\/wiki\\/.*");
+    public static void initialize() throws IOException, MalformedURLException, ParseException {
+        File links = new File("links.txt");
+        Scanner file = new Scanner(links);
+        while (file.hasNextLine()){
+            Document doc = (handleUrl(file.nextLine()));
+            Elements linkElements = doc.select("a");
+            for (Element e:
+                 linkElements) {
+                String link = (e.attr("href"));
+                if(!link.contains("File:")){
+                    Matcher m = URL_PATTERN.matcher(link);
+                    if(m.matches()){
+                        String flink = BASE_URI + (e.attr("href"));
+                        System.out.println(flink);
+                    }
+                }
+
+
+            }
+        }
+    }
     public static Document handleUrl(String url) throws IOException, MalformedURLException, ParseException {
         int fileName = url.hashCode();
         File dir = new File("cache");
@@ -68,8 +98,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
+        Button initialize = new Button("Initialize");
         TextField urlField = new TextField();
         Label label = new Label("Enter URL:");
         btn.setText("Enter");
@@ -89,11 +121,25 @@ public class Main extends Application {
                 }
             }
         });
+        initialize.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try{
+                    initialize();
+                }catch (IOException ioe){
+                    ioe.printStackTrace();
 
+                }
+                catch (ParseException p){
+                    p.printStackTrace();
+                }
+            }
+        });
         GridPane root = new GridPane();
         GridPane.setConstraints(btn,1,0);
         GridPane.setConstraints(urlField,0,0);
-        root.getChildren().addAll(btn,urlField);
+        GridPane.setConstraints(initialize,0,2);
+        root.getChildren().addAll(btn,urlField,initialize);
         primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
