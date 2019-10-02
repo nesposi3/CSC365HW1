dcf716d3b5f1eac671d2a32a5e1292d983c9d607
Nicholas Esposito <nesposi3@oswego.edu>
2019-10-02 19:12:46 -0400

    Updated GUI so it is centered

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,9 @@ package main;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.jsoup.nodes.Document;
@@ -38,7 +42,7 @@ public class Main extends Application {
         Button btn = new Button();
         TextField urlField = new TextField();
         Text closest = new Text();
-        closest.setText(" Enter a wikipedia url to determine its similarity to the cached files!");
+        closest.setText("Enter a wikipedia url to determine its similarity to the cached files!");
         btn.setText("Enter");
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -61,13 +65,18 @@ public class Main extends Application {
             }
         });
         GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        GridPane.setMargin(closest,new Insets(5,5,5,5));
+        closest.setFont(new Font(12));
         GridPane.setValignment(btn, VPos.CENTER);
         GridPane.setValignment(urlField, VPos.CENTER);
-        GridPane.setConstraints(btn,2,0);
-        GridPane.setConstraints(urlField,1,0);
-        GridPane.setConstraints(closest,1,1);
+        GridPane.setHalignment(btn, HPos.CENTER);
+        GridPane.setHalignment(urlField,HPos.CENTER);
+        GridPane.setConstraints(btn,1,1);
+        GridPane.setConstraints(urlField,0,1);
+        GridPane.setConstraints(closest,0,0);
         root.getChildren().addAll(btn,urlField,closest);
-        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 }
